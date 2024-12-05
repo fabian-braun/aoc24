757ebeac210817b9c08ededf5a4c1e295e757364
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-05 09:46:02 +0100

    Implement day 5 part 2, topo sort, not working yet

--- crates/day5/src/main.rs
+++ crates/day5/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
-    let content = utilities::get_input(5).await;
+    let content = utilities::get_example(5).await;
     let mut s: HashMap<i64, Vec<i64>> = hashmap! {};
     let mut p: HashMap<i64, Vec<i64>> = hashmap! {};
     let mut updates = vec![];
@@ -30,17 +30,34 @@ async fn main() {
         }
     }
 
+    // topo sort
+    let mut sorted = vec![];
+    let mut rem = p.clone();
+    loop {
+        if rem.is_empty() {
+            break;
+        }
+        let next = *rem.iter().find(|(s, ps)| ps.is_empty()).unwrap().0;
+        rem.remove(&next);
+        rem.values_mut()
+            .for_each(|ps| ps.retain(|elem| elem != &next));
+        sorted.push(next);
+    }
+
     let mut result = 0i64;
-    for update in updates {
+    for mut update in updates {
         let is_valid = update
             .iter()
             .tuple_combinations()
             .all(|(a, b)| !p[a].contains(b));
         if is_valid {
+            // let mid = update[update.len() / 2];
+            // result += mid;
+        } else {
+            update.sort_by_key(|a| sorted.iter().position(|x| x == a).unwrap());
+            println!("{:?}", update);
             let mid = update[update.len() / 2];
             result += mid;
-        } else {
-
         }
     }
 
