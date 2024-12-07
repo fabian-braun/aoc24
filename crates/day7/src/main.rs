d931b1c22749bbdc8fec4860269518f1b9e6bccf
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-07 07:28:41 +0100

    Solve day 7 part 2, wrong (precedence constraint)

--- crates/day7/src/main.rs
+++ crates/day7/src/main.rs
@@ -1,6 +1,4 @@
 use itertools::Itertools;
-use ndarray::Axis;
-use utilities::char_matrix;
 
 const VERSION: &str = env!("CARGO_PKG_NAME");
 
@@ -13,8 +11,8 @@ async fn main() {
         .unwrap_or(1);
     let content = utilities::get_example(day).await;
     println!("Example Solution for day {}: \n{:?}\n", day, run(content));
-    let content = utilities::get_input(day).await;
-    println!("Actual Solution for day {}: \n{:?}\n", day, run(content));
+    // let content = utilities::get_input(day).await;
+    // println!("Actual Solution for day {}: \n{:?}\n", day, run(content));
 }
 
 fn run(input: String) -> anyhow::Result<String> {
@@ -28,9 +26,11 @@ fn run(input: String) -> anyhow::Result<String> {
                 .split_ascii_whitespace()
                 .map(|x| x.parse().unwrap())
                 .collect_vec();
-            if dfs(tv, rhs[0], &rhs, 1) {
+            if dfs(tv, 0_i64, &rhs, 0) {
+                println!("✅ {}", line);
                 Some(tv)
             } else {
+                println!("❌ {}", line);
                 None
             }
         })
@@ -38,19 +38,33 @@ fn run(input: String) -> anyhow::Result<String> {
     Ok(result.to_string())
 }
 
-fn dfs(tv_target: i64, tv_current: i64, rhs: &[i64], idx: usize) -> bool {
-    if tv_current > tv_target {
+fn dfs(tgt: i64, tv_c: i64, rhs: &[i64], i: usize) -> bool {
+    if tv_c > tgt || i >= rhs.len() {
         false
-    } else if idx == rhs.len() - 1 {
-        tv_current + rhs[idx] == tv_target || tv_current * rhs[idx] == tv_target
+    } else if i == rhs.len() - 2
+        && (tv_c + cc(rhs[i], rhs[i + 1]) == tgt || tv_c * cc(rhs[i], rhs[i + 1]) == tgt)
+    {
+        true
+    } else if i == rhs.len() - 1 {
+        tv_c + rhs[i] == tgt || tv_c * rhs[i] == tgt
     } else {
-        dfs(tv_target, tv_current + rhs[idx], rhs, idx + 1)
-            || dfs(tv_target, tv_current * rhs[idx], rhs, idx + 1)
+        dfs(tgt, tv_c + rhs[i], rhs, i + 1)
+            || dfs(tgt, tv_c * rhs[i], rhs, i + 1)
+            || dfs(tgt, tv_c + cc(rhs[i], rhs[i + 1]), rhs, i + 2)
+            || dfs(tgt, tv_c * cc(rhs[i], rhs[i + 1]), rhs, i + 2)
     }
+}
+
+fn cc(a: i64, b: i64) -> i64 {
+    let len_b = b.checked_ilog10().unwrap_or(0) + 1;
+    a * 10_i64.pow(len_b) + b
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn test_something() {}
+    fn test_something() {
+        assert_eq!(12233324324, cc(1223, 3324324))
+    }
 }
