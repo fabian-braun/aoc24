bde37a6a494bf7d337a05a6c735d888d7d7e17f6
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-08 06:58:29 +0100

    use nd array

--- crates/dayX/src/main.rs
+++ crates/dayX/src/main.rs
@@ -1,10 +1,10 @@
 use itertools::Itertools;
-use maplit::hashset;
-use ndarray::Axis;
+use ndarray::{Array2, Axis};
 use std::collections::HashMap;
 use utilities::char_matrix;
 
 const VERSION: &str = env!("CARGO_PKG_NAME");
+pub type B = Array2<bool>;
 
 #[tokio::main]
 async fn main() {
@@ -30,15 +30,15 @@ fn run(input: String) -> anyhow::Result<String> {
             _ => Some((c, (y, x))),
         })
         .into_group_map();
-    let mut unique_locations = hashset! {};
+    let mut unique_locations = B::default((y_len as usize, x_len as usize));
     antenna_groups.into_iter().for_each(|(_c, coords)| {
         for ((y1, x1), (y2, x2)) in coords.iter().tuple_combinations() {
+            unique_locations[(*y1, *x1)] = true;
+            unique_locations[(*y2, *x2)] = true;
             let y1 = *y1 as i64;
             let x1 = *x1 as i64;
             let y2 = *y2 as i64;
             let x2 = *x2 as i64;
-            unique_locations.insert((y1, x1));
-            unique_locations.insert((y2, x2));
             let dy: i64 = y1 - y2;
             let dx: i64 = x1 - x2;
             let mut y3: i64 = y1 + dy;
@@ -46,18 +46,21 @@ fn run(input: String) -> anyhow::Result<String> {
             let mut y4: i64 = y2 - dy;
             let mut x4: i64 = x2 - dx;
             while y3 >= 0 && y3 < y_len && x3 >= 0 && x3 < x_len {
-                unique_locations.insert((y3, x3));
+                unique_locations[(y3 as usize, x3 as usize)] = true;
                 y3 = y3 + dy;
                 x3 = x3 + dx;
             }
             while y4 >= 0 && y4 < y_len && x4 >= 0 && x4 < x_len {
-                unique_locations.insert((y4, x4));
+                unique_locations[(y4 as usize, x4 as usize)] = true;
                 y4 = y4 - dy;
                 x4 = x4 - dx;
             }
         }
     });
-    Ok(format!("{:?}", unique_locations.len()))
+    Ok(format!(
+        "{:?}",
+        unique_locations.iter().filter(|x| **x).count()
+    ))
 }
 
 #[cfg(test)]
