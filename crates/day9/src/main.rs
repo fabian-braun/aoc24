e07fb82e4c37164398d7e8939efe0c34178216bb
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-09 06:55:32 +0100

    Implement day 9 part 1

--- crates/day9/src/main.rs
+++ crates/day9/src/main.rs
@@ -1,4 +1,5 @@
 use ndarray::Axis;
+use std::u32;
 use utilities::char_matrix;
 
 const VERSION: &str = env!("CARGO_PKG_NAME");
@@ -17,9 +18,39 @@ async fn main() {
 }
 
 fn run(input: String) -> anyhow::Result<String> {
-    let matrix = char_matrix(input)?;
-    let result: usize = matrix.len_of(Axis(0)) * matrix.len_of(Axis(1));
-    Ok(result.to_string())
+    let mut file = vec![];
+    let mut id = 0_u16;
+    let mut space = false;
+    let input = input.split_ascii_whitespace().next().unwrap();
+    input.chars().for_each(|c| {
+        let count: u8 = format!("{}", c).parse().unwrap_or(0);
+        if space {
+            for _ in 0..count {
+                file.push(u16::MAX);
+            }
+        } else {
+            for _ in 0..count {
+                file.push(id);
+            }
+            id += 1;
+        }
+        space = !space;
+    });
+    let mut lc = 0;
+    let mut rc = file.len() - 1;
+    while lc < rc {
+        while file[lc] != u16::MAX && lc < rc {
+            lc += 1;
+        }
+        while file[rc] == u16::MAX && lc < rc {
+            rc -= 1;
+        }
+        file.swap(lc, rc);
+        lc += 1;
+        rc -= 1;
+    }
+    let checksum: usize = file.iter().filter(|x| **x != u16::MAX).enumerate().map(|(idx, id)| idx * (*id as usize)).sum();
+    Ok(format!("{:?}", checksum))
 }
 
 #[cfg(test)]
