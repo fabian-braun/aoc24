1adc05791077fdb94d37319fab296319a6ed9ff1
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-15 16:17:35 +0100

    Solve day 15 part 2

--- crates/day15/src/main.rs
+++ crates/day15/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use ndarray::Axis;
 use std::time::Instant;
 use utilities::{char_matrix, M};
 
@@ -35,31 +36,81 @@ fn run(input: String) -> anyhow::Result<String> {
             _ => None,
         })
         .collect_vec();
-    let mut m = char_matrix(map.to_string())?;
+    let m = char_matrix(map.to_string())?;
+    let mut m = expand(m);
     let mut pos = m
         .indexed_iter()
         .find_or_first(|(_, &c)| c == '@')
         .unwrap()
         .0;
     for mv in movement {
-        if push(pos, mv, &mut m) {
-            pos = (
-                pos.0.checked_add_signed(mv.0).unwrap(),
-                pos.1.checked_add_signed(mv.1).unwrap(),
-            )
+        if mv.0 == 0 {
+            // horizontal move
+            if push_h(pos, mv, &mut m) {
+                pos = (
+                    pos.0.checked_add_signed(mv.0).unwrap(),
+                    pos.1.checked_add_signed(mv.1).unwrap(),
+                )
+            }
+        } else {
+            let d = mv.0;
+            // vertical move
+            if can_push_v(pos, d, &m) {
+                push_v(&[pos], d, &mut m);
+                pos = (pos.0.checked_add_signed(d).unwrap(), pos.1)
+            }
         }
     }
-    let result: usize = m.indexed_iter().map(|((y, x), &c)| {
-        if c == 'O' {
-            100_usize * y + x
-        } else {
-            0
-        }
-    }).sum();
+    let result: usize = m
+        .indexed_iter()
+        .map(|((y, x), &c)| if c == '[' { 100_usize * y + x } else { 0 })
+        .sum();
     Ok(result.to_string())
 }
 
-fn push(pos: (usize, usize), d: (isize, isize), m: &mut M) -> bool {
+fn can_push_v(pos: (usize, usize), d: isize, m: &M) -> bool {
+    assert_ne!(d, 0);
+    let tgt = match m[pos] {
+        '[' => vec![
+            (pos.0.checked_add_signed(d).unwrap(), pos.1),
+            (pos.0.checked_add_signed(d).unwrap(), pos.1 + 1),
+        ],
+        ']' => vec![
+            (pos.0.checked_add_signed(d).unwrap(), pos.1),
+            (pos.0.checked_add_signed(d).unwrap(), pos.1 - 1),
+        ],
+        '@' => vec![(pos.0.checked_add_signed(d).unwrap(), pos.1)],
+        '.' => return true,
+        _ => return false,
+    };
+    tgt.iter().all(|tgt| can_push_v(*tgt, d, m))
+}
+
+fn push_v(pos: &[(usize, usize)], d: isize, m: &mut M) {
+    assert_ne!(d, 0);
+    let push_next = pos
+        .iter()
+        .map(|&pos| {
+            let tgt = (pos.0.checked_add_signed(d).unwrap(), pos.1);
+            match m[tgt] {
+                '[' => vec![(tgt.0, tgt.1), (tgt.0, tgt.1 + 1)],
+                ']' => vec![(tgt.0, tgt.1 - 1), (tgt.0, tgt.1)],
+                _ => vec![],
+            }
+        })
+        .flatten()
+        .unique()
+        .collect_vec();
+    if !push_next.is_empty() {
+        push_v(&push_next, d, m);
+    }
+    pos.iter().for_each(|pos| {
+        m[(pos.0.checked_add_signed(d).unwrap(), pos.1)] = m[*pos];
+        m[*pos] = '.';
+    });
+}
+
+fn push_h(pos: (usize, usize), d: (isize, isize), m: &mut M) -> bool {
     let tgt = (
         pos.0.checked_add_signed(d.0).unwrap(),
         pos.1.checked_add_signed(d.1).unwrap(),
@@ -70,8 +121,17 @@ fn push(pos: (usize, usize), d: (isize, isize), m: &mut M) -> bool {
             m[pos] = '.';
             true
         }
-        'O' => {
-            if push(tgt, d, m) {
+        '[' => {
+            if push_h(tgt, d, m) {
+                m[tgt] = m[pos];
+                m[pos] = '.';
+                true
+            } else {
+                false
+            }
+        }
+        ']' => {
+            if push_h(tgt, d, m) {
                 m[tgt] = m[pos];
                 m[pos] = '.';
                 true
@@ -83,6 +143,33 @@ fn push(pos: (usize, usize), d: (isize, isize), m: &mut M) -> bool {
     }
 }
 
+fn expand(m: M) -> M {
+    let mut e = M::default((m.len_of(Axis(0)), m.len_of(Axis(0)) * 2));
+    m.indexed_iter().for_each(|((y, x), &c)| {
+        let e_x = x * 2;
+        match c {
+            '#' => {
+                e[(y, e_x)] = '#';
+                e[(y, e_x + 1)] = '#';
+            }
+            'O' => {
+                e[(y, e_x)] = '[';
+                e[(y, e_x + 1)] = ']';
+            }
+            '.' => {
+                e[(y, e_x)] = '.';
+                e[(y, e_x + 1)] = '.';
+            }
+            '@' => {
+                e[(y, e_x)] = '@';
+                e[(y, e_x + 1)] = '.';
+            }
+            _ => {}
+        }
+    });
+    e
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -92,22 +179,8 @@ mod tests {
             content: &'static str,
             expected: &'static str,
         }
-        let examples = [
-            Example {
-                content: "########
-#..O.O.#
-##@.O..#
-#...O..#
-#.#.O..#
-#...O..#
-#......#
-########
-
-<^^>>>vv<v>>v<<",
-                expected: "2028",
-            },
-            Example {
-                content: "##########
+        let examples = [Example {
+            content: "##########
 #..O..O.O#
 #......O.#
 #.OO..O.O#
@@ -128,9 +201,8 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
-                expected: "10092",
-            },
-        ];
+            expected: "9021",
+        }];
         for (i, ex) in examples.iter().enumerate() {
             assert_eq!(
                 ex.expected.to_string(),
