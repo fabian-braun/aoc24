643157c09f36ba587ce2aae1caed7dc4d1dae885
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-16 17:16:16 +0100

    Solve day 16 intermediate state part 2

--- crates/day16/src/main.rs
+++ crates/day16/src/main.rs
@@ -1,9 +1,9 @@
-use maplit::hashmap;
+use maplit::{hashmap, hashset};
 use ndarray::Axis;
 use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::time::Instant;
-use utilities::char_matrix;
+use utilities::{char_matrix, I, M};
 
 const VERSION: &str = env!("CARGO_PKG_NAME");
 
@@ -45,28 +45,68 @@ fn run(input: String) -> anyhow::Result<String> {
     let m = char_matrix(input)?;
     let y_len = m.len_of(Axis(0));
     let x_len = m.len_of(Axis(1));
+    let mut c_m = I::default((y_len, x_len));
     let start: (u64, (usize, usize), (isize, isize)) = (0, (y_len - 2, 1), EAST);
     let end = (1, x_len - 2);
     println!("search path from {start:?} to {end:?}");
     let mut min_cost = hashmap! { start.1 => start.0 };
+    let mut pred: HashMap<(usize, usize), HashSet<(usize, usize)>> = hashmap! {};
     let mut heap = BinaryHeap::new();
     heap.push(Reverse(start));
     while let Some(Reverse((cost, pos, d))) = heap.pop() {
-        if pos == end {
-            break;
-        }
         if cost > min_cost[&pos] {
             continue;
         }
         neighbours(cost, pos, d).into_iter().for_each(|n| {
-            if n.0 < *min_cost.get(&n.1).unwrap_or(&u64::MAX) && (m[n.1] == '.' || m[n.1] == 'E' ) {
+            if n.0 < *min_cost.get(&n.1).unwrap_or(&u64::MAX) && (m[n.1] == '.' || m[n.1] == 'E') {
                 heap.push(Reverse(n));
                 min_cost.insert(n.1, n.0);
+                c_m[n.1] = n.0 as usize;
+                pred.entry(n.1).or_default().insert(pos);
             }
         })
     }
-    Ok(min_cost[&end].to_string())
+    print(&m, &c_m);
+
+    let mut tiles = hashset![];
+    let mut curr = vec![end];
+    while let Some(c) = curr.pop() {
+        tiles.insert(c);
+        let pred = pred.get(&c);
+        pred.map(|pred| pred.iter().for_each(|p| curr.push(*p)));
+    }
+    Ok(tiles.len().to_string())
 }
+
+fn count_tiles(
+    current: (u64, (usize, usize), (isize, isize)),
+    min_cost: &HashMap<(usize, usize), u64>,
+    m: &M,
+) -> usize {
+    if current.0 > min_cost[&current.1] {
+        0
+    } else if m[current.1] == 'E' {
+        1
+    } else {
+        neighbours(current.0, current.1, current.2)
+            .into_iter()
+            .map(|n| {
+                if (m[n.1] == '.' || m[n.1] == 'E') && n.0 <= min_cost[&n.1] {
+                    let tiles = count_tiles(n, &min_cost, m);
+                    if tiles > 0 {
+                        println!("{:?}", n.1);
+                        tiles + 1
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                }
+            })
+            .sum()
+    }
+}
+
 fn neighbours(
     cost: u64,
     x: (usize, usize),
@@ -96,6 +136,21 @@ fn neighbours(
     n
 }
 
+fn print(m: &M, c_m: &I) {
+    for y in 0..c_m.len_of(Axis(0)) {
+        println!();
+        for x in 0..c_m.len_of(Axis(1)) {
+            if m[(y, x)] == '.' || m[(y, x)] == 'E' {
+                print!(" {:5?}", c_m[(y, x)]);
+            } else {
+                print!("      ");
+            }
+
+        }
+    }
+    println!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -122,28 +177,28 @@ mod tests {
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############",
-                expected: "7036",
+                expected: "45",
             },
-            Example {
-                content: "#################
-#...#...#...#..E#
-#.#.#.#.#.#.#.#.#
-#.#.#.#...#...#.#
-#.#.#.#.###.#.#.#
-#...#.#.#.....#.#
-#.#.#.#.#.#####.#
-#.#...#.#.#.....#
-#.#.#####.#.###.#
-#.#.#.......#...#
-#.#.###.#####.###
-#.#.#...#.....#.#
-#.#.#.#####.###.#
-#.#.#.........#.#
-#.#.#.#########.#
-#S#.............#
-#################",
-                expected: "11048",
-            },
+            //             Example {
+            //                 content: "#################
+            // #...#...#...#..E#
+            // #.#.#.#.#.#.#.#.#
+            // #.#.#.#...#...#.#
+            // #.#.#.#.###.#.#.#
+            // #...#.#.#.....#.#
+            // #.#.#.#.#.#####.#
+            // #.#...#.#.#.....#
+            // #.#.#####.#.###.#
+            // #.#.#.......#...#
+            // #.#.###.#####.###
+            // #.#.#...#.....#.#
+            // #.#.#.#####.###.#
+            // #.#.#.........#.#
+            // #.#.#.#########.#
+            // #S#.............#
+            // #################",
+            //                 expected: "64",
+            //             },
         ];
         for (i, ex) in examples.iter().enumerate() {
             assert_eq!(
