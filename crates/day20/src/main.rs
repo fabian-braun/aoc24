72579ab3f766868d6a0180aa1a1ac9228a226a1c
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-20 07:36:32 +0100

    day 20 part 1, shortest path

--- crates/day20/src/main.rs
+++ crates/day20/src/main.rs
@@ -1,6 +1,10 @@
-use std::time::Instant;
+use anyhow::Context;
+use maplit::{hashmap, hashset};
 use ndarray::Axis;
-use utilities::char_matrix;
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap};
+use std::time::Instant;
+use utilities::M;
 
 const VERSION: &str = env!("CARGO_PKG_NAME");
 
@@ -17,13 +21,99 @@ async fn main() {
     let start = Instant::now();
     let solution = run(content);
     let time_taken = start.elapsed();
-    println!("Actual Solution for day {}: \n{:?}\nin time {:?}", day, solution, time_taken);
+    println!(
+        "Actual Solution for day {}: \n{:?}\nin time {:?}",
+        day, solution, time_taken
+    );
 }
 
 fn run(input: String) -> anyhow::Result<String> {
-    let matrix = char_matrix(input)?;
-    let result: usize = matrix.len_of(Axis(0)) * matrix.len_of(Axis(1));
-    Ok(result.to_string())
+    let (m, start, end) = char_matrix(input)?;
+
+    print(&m);
+    let mut min_cost = hashmap! { start => 0_u64 };
+    let mut pred: HashMap<(usize, usize), (usize, usize)> = hashmap! {};
+    let mut heap = BinaryHeap::<Reverse<(u64, (usize, usize))>>::new();
+    heap.push(Reverse((0u64, start)));
+    while let Some(Reverse((cost, pos))) = heap.pop() {
+        if cost > min_cost[&pos] {
+            continue;
+        }
+        let new_cost: u64 = cost + 1;
+        neighbours(pos).into_iter().for_each(|n| {
+            if new_cost < *min_cost.get(&n).unwrap_or(&u64::MAX) && (m[n] == '.' || m[n] == 'E') {
+                heap.push(Reverse((new_cost, n)));
+                min_cost.insert(n, new_cost);
+                pred.insert(n, pos);
+            }
+        })
+    }
+
+    let mut s_p = hashset![end];
+    let mut curr = end;
+    while let Some(c) = pred.get(&curr) {
+        s_p.insert(*c);
+        curr = *c
+    }
+
+    Ok(3.to_string())
+}
+
+fn neighbours(x: (usize, usize)) -> [(usize, usize); 4] {
+    [
+        (x.0, x.1 + 1),
+        (x.0, x.1 - 1),
+        (x.0 + 1, x.1),
+        (x.0 - 1, x.1),
+    ]
+}
+fn print(m: &M) {
+    for y in 0..m.len_of(Axis(0)) {
+        println!();
+        for x in 0..m.len_of(Axis(1)) {
+            print!("{}", m[(y, x)]);
+        }
+    }
+    println!();
+}
+
+fn char_matrix(raw: String) -> anyhow::Result<(M, (usize, usize), (usize, usize))> {
+    let y_len = raw.lines().filter(|line| !line.is_empty()).count();
+    let x_len = raw.lines().next().context("char_matrix")?.len();
+    let mut m = M::default((y_len, x_len));
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+
+    raw.lines()
+        .filter(|line| !line.is_empty())
+        .enumerate()
+        .for_each(|(y, line)| {
+            line.chars().enumerate().for_each(|(x, c)| {
+                if x == 0 || y == 0 || x == x_len - 1 || y == y_len - 1 {
+                    if c != '#' {
+                        panic!()
+                    }
+                    m[(y, x)] = 'O';
+                } else {
+                    if c == 'S' {
+                        start = (y, x);
+                        m[(y, x)] = c;
+                    } else if c == 'E' {
+                        end = (y, x);
+                        m[(y, x)] = c;
+                    } else {
+                        m[(y, x)] = c;
+                    }
+                }
+            })
+        });
+    if start == (0, 0) {
+        panic!("start not found")
+    }
+    if end == (0, 0) {
+        panic!("end not found")
+    }
+    Ok((m, start, end))
 }
 
 #[cfg(test)]
