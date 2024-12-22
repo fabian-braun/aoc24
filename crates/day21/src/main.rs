dbb4de6c5b04efd7d6b8b15d2caa6e27c10fe26d
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-22 23:40:20 +0100

    WIP day 21 part 1, distance_directional_kp working

--- crates/day21/src/main.rs
+++ crates/day21/src/main.rs
@@ -50,7 +50,7 @@ fn run(input: String) -> anyhow::Result<String> {
 
             let mut next_next_chars: Vec<char> = vec![];
             next_chars.into_iter().tuple_windows().for_each(|(c1, c2)| {
-                next_next_chars.extend(dir_lookup[dir_idx(c1)][dir_idx(c2)].iter());
+                next_next_chars.extend(dir_lookup[dix(c1)][dix(c2)].iter());
             });
             println!("robot 2 {:?}", next_next_chars.iter().join(""));
             next_next_chars.insert(0, 'A');
@@ -60,7 +60,7 @@ fn run(input: String) -> anyhow::Result<String> {
                 .into_iter()
                 .tuple_windows()
                 .for_each(|(c1, c2)| {
-                    next_next_next_chars.extend(dir_lookup[dir_idx(c1)][dir_idx(c2)].iter());
+                    next_next_next_chars.extend(dir_lookup[dix(c1)][dix(c2)].iter());
                 });
             println!("robot 3 {:?}", next_next_next_chars.iter().join(""));
             println!(
@@ -79,7 +79,7 @@ fn run(input: String) -> anyhow::Result<String> {
 fn num_idx(c: char) -> usize {
     c.to_digit(11).unwrap() as usize
 }
-const fn dir_idx(c: char) -> usize {
+const fn dix(c: char) -> usize {
     match c {
         '<' => 0,
         '>' => 1,
@@ -176,30 +176,30 @@ fn compute_distance_directional_kp(depth: usize) -> Vec<Vec<usize>> {
                             continue;
                         }
                         let mut min_cost = vec![usize::MAX; 5];
-                        min_cost[dir_idx(org)] = 0;
+                        min_cost[dix(org)] = 0;
                         let mut heap = BinaryHeap::new();
                         heap.push(Reverse((0, org)));
                         while let Some(Reverse((c, pos))) = heap.pop() {
                             if pos == dst {
                                 break;
                             }
-                            if c > min_cost[dir_idx(pos)] {
+                            if c > min_cost[dix(pos)] {
                                 continue;
                             }
-                            for n in KPN[dir_idx(pos)] {
+                            for n in KPN[dix(pos)] {
                                 let new_cost = c
-                                    + cost[dir_idx(pos)][dir_idx(*n)]
-                                    + cost[dir_idx(*n)][dir_idx('A')];
-                                if new_cost < min_cost[dir_idx(*n)] {
+                                    + cost[dix(pos)][dix(*n)]
+                                    + cost[dix(*n)][dix('A')];
+                                if new_cost < min_cost[dix(*n)] {
                                     heap.push(Reverse((new_cost, *n)));
-                                    min_cost[dir_idx(*n)] = new_cost;
+                                    min_cost[dix(*n)] = new_cost;
                                 }
                             }
                         }
 
-                        let orig = dir_idx(org);
-                        let dest = dir_idx(dst);
-                        cost_n[orig][dest] = min_cost[dir_idx(dst)];
+                        let orig = dix(org);
+                        let dest = dix(dst);
+                        cost_n[orig][dest] = min_cost[dix(dst)];
                     }
                 }
             }
@@ -221,8 +221,8 @@ fn compute_sequences_directional_kp() -> Vec<Vec<Vec<char>>> {
                     if m[(y_org, x_org)] == 'X' || m[(y_dst, x_dst)] == 'X' {
                         continue;
                     }
-                    let orig = dir_idx(m[(y_org, x_org)]);
-                    let dest = dir_idx(m[(y_dst, x_dst)]);
+                    let orig = dix(m[(y_org, x_org)]);
+                    let dest = dix(m[(y_dst, x_dst)]);
                     let mut dy: isize = y_dst as isize - y_org as isize;
                     let mut dx: isize = x_dst as isize - x_org as isize;
                     let mut v = vec![];
@@ -277,22 +277,22 @@ mod tests {
     fn test_directional_kp() {
         let lookup = compute_sequences_directional_kp();
         for c in ['A', '<', '^', '>', 'v'] {
-            assert_eq!(&vec!['A'], &lookup[dir_idx(c)][dir_idx(c)]);
+            assert_eq!(&vec!['A'], &lookup[dix(c)][dix(c)]);
         }
-        assert_eq!(&vec!['<', 'A'], &lookup[dir_idx('A')][dir_idx('^')]);
-        assert_eq!(&vec!['v', '<', 'A'], &lookup[dir_idx('^')][dir_idx('<')]);
-        assert_eq!(&vec!['>', '^', 'A'], &lookup[dir_idx('<')][dir_idx('^')]);
+        assert_eq!(&vec!['<', 'A'], &lookup[dix('A')][dix('^')]);
+        assert_eq!(&vec!['v', '<', 'A'], &lookup[dix('^')][dix('<')]);
+        assert_eq!(&vec!['>', '^', 'A'], &lookup[dix('<')][dix('^')]);
     }
 
     #[test]
     fn test_compute_distance_directional_kp() {
         let lookup = compute_distance_directional_kp(1);
         for c in ['A', '<', '^', '>', 'v'] {
-            assert_eq!(0, lookup[dir_idx(c)][dir_idx(c)]);
+            assert_eq!(0, lookup[dix(c)][dix(c)]);
         }
-        assert_eq!(2, lookup[dir_idx('A')][dir_idx('>')]);
-        assert_eq!(2, lookup[dir_idx('A')][dir_idx('^')]);
-        assert_eq!(4, lookup[dir_idx('A')][dir_idx('v')]);
-        assert_eq!(6, lookup[dir_idx('A')][dir_idx('<')]);
+        assert_eq!(2, lookup[dix('A')][dix('>')]);
+        assert_eq!(2, lookup[dix('A')][dix('^')]);
+        assert_eq!(4, lookup[dix('A')][dix('v')]);
+        assert_eq!(6, lookup[dix('A')][dix('<')]);
     }
 }
