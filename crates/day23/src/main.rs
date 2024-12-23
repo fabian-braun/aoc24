2e9dba66a868b924c452f165a10db4f9255e2099
Fabian Braun <fabian-braun@users.noreply.github.com>
2024-12-23 07:49:08 +0100

    WIP day 23 part 3

--- crates/day23/src/main.rs
+++ crates/day23/src/main.rs
@@ -1,4 +1,6 @@
 use itertools::Itertools;
+use maplit::hashset;
+use std::collections::HashSet;
 use std::time::Instant;
 use utilities::B;
 
@@ -27,20 +29,17 @@ async fn main() {
 
 fn run(input: String) -> anyhow::Result<String> {
     let mut nodes_s: Vec<String> = vec![];
-    let mut nodes: Vec<usize> = vec![];
     let mut edges: Vec<(usize, usize)> = vec![];
     input.lines().filter(|l| !l.is_empty()).for_each(|line| {
         let (a, b) = line.split_once('-').unwrap();
         let mut a_idx = nodes_s.iter().position(|x| x == a);
         if a_idx.is_none() {
             a_idx = Some(nodes_s.len());
-            nodes.push(a_idx.unwrap());
             nodes_s.push(a.to_string());
         }
         let mut b_idx = nodes_s.iter().position(|x| x == b);
         if b_idx.is_none() {
             b_idx = Some(nodes_s.len());
-            nodes.push(b_idx.unwrap());
             nodes_s.push(b.to_string());
         }
         if !edges.contains(&(a_idx.unwrap(), b_idx.unwrap())) {
@@ -51,31 +50,59 @@ fn run(input: String) -> anyhow::Result<String> {
     for (a, b) in &edges {
         m[(*a, *b)] = true;
         m[(*b, *a)] = true;
-        m[(*a, *a)] = true;
-        m[(*b, *b)] = true;
     }
-    let subgraphs = nodes.clone();
-    let mut subgraphs_2 = vec![];
+    let subgraphs = (0..nodes_s.len()).collect_vec();
+    let mut subgraphs_2: Vec<Vec<bool>> = vec![];
     for (a, b) in subgraphs.iter().tuple_combinations() {
         if m[(*a, *b)] {
-            subgraphs_2.push(vec![a, b]);
+            let mut x = vec![false; nodes_s.len()];
+            x[*a] = true;
+            x[*b] = true;
+            subgraphs_2.push(x);
         }
     }
-    let mut subgraphs_3 = vec![];
-    for (a, b) in subgraphs_2.iter().tuple_combinations() {
-        let mut merged = a.iter().chain(b.iter()).unique().collect_vec();
-        if merged.len() == 3 && a.iter().all(|&a| b.iter().all(|&b| m[(*a, *b)])) {
-            merged.sort_unstable();
-            if !subgraphs_3.iter().contains(&merged) {
-                subgraphs_3.push(merged);
+    let mut subgraphs_prev = subgraphs_2;
+    for i in 3..100 {
+        dbg!(subgraphs_prev.len());
+        let mut subgraphs_next: Vec<Vec<bool>> = vec![];
+        for a in subgraphs_prev.iter() {
+            for b in 0..nodes_s.len() {
+                if a.iter()
+                    .enumerate()
+                    .filter(|(_ax, is_in)| **is_in)
+                    .all(|(ax, _is_in)| m[(ax, b)])
+                {
+                    let mut merged = a.clone();
+                    merged[b] = true;
+                    // if !subgraphs_next.iter().contains(&merged) {
+                    subgraphs_next.push(merged);
+                    // }
+                }
             }
         }
+        if subgraphs_next.len() == 0 {
+            break;
+        }
+        subgraphs_prev = subgraphs_next;
     }
-    let result = subgraphs_3
+
+    let mut result = subgraphs_prev
         .iter()
-        .filter(|sg| sg.iter().any(|n_idx| nodes_s[***n_idx].starts_with('t')))
-        .count();
-    Ok(result.to_string())
+        .next()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter_map(|(n_idx, is_in)| {
+            if is_in {
+                Some(nodes_s[n_idx].clone())
+            } else {
+                None
+            }
+        })
+        .collect_vec();
+    result.sort_unstable();
+    Ok(result.into_iter().join(","))
 }
 
 #[cfg(test)]
